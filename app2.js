b11d21829d4e4b12a472f5f08aff63e9631d1b7d
Ernest <soquena.305519@stamaria.sti.edu.ph>
2024-12-05 15:16:42 +0800

    asdad

--- app2.js
+++ app2.js
@@ -11,7 +11,7 @@ const calendar = document.querySelector(".calendar"),
   eventsContainer = document.querySelector(".events"),
   addEventBtn = document.querySelector(".add-event"),
   addEventWrapper = document.querySelector(".add-event-wrapper "),
-  addEventCloseBtn = document.querySelector(".close "),
+  addEventCloseBtn = document.querySelector(".close"),
   addEventTitle = document.querySelector(".event-name "),
   addEventFrom = document.querySelector(".event-time-from "),
   addEventTo = document.querySelector(".event-time-to "),
@@ -36,7 +36,23 @@ const months = [
     "November",
     "December",
 ];
-
+const eventsArr = [
+    {
+        day: 5,
+        month: 12,
+        year: 2024,
+        events:[
+            {
+                title: "Event 1",
+                time:"10:00AM"
+            },
+            {
+                title:"Event 2",
+                time:"11:00AM"
+            }
+        ]
+    }
+]
 function initCalendar() {
     const firstDay = new Date(year, month, 1);
     const lastDay = new Date(year, month + 1, 0);
@@ -54,24 +70,45 @@ function initCalendar() {
         days += `<div class="day prev-date">${prevDays - x + 1}</div>`;
       }
     
-    for(let i = 1; i <= lastDate; i++){
-        if (
-            i === new Date().getDate() &&
-            year === new Date().getFullYear() &&
-            month === new Date().getMonth()
+      for (let i = 1; i <= lastDate; i++) {
+        
+        let event = false;
+        eventsArr.forEach((eventObj) => {
+          if (
+            eventObj.day === i &&
+            eventObj.month === month + 1 &&
+            eventObj.year === year
           ) {
-            days += `<div class="day today">${i}</div>`;
-        }
-        else{
-            days += `<div class="day">${i}</div>`;
-        }
-    }
+            event = true;
+          }
+        });
 
+        if (
+          i === new Date().getDate() &&
+          year === new Date().getFullYear() &&
+          month === new Date().getMonth()
+        ) {
+          if (event) {
+            days += `<div class="day today  event">${i}</div>`;
+          } else {
+            days += `<div class="day today ">${i}</div>`;
+          }
+        } else {
+          if (event) {
+            days += `<div class="day event">${i}</div>`;
+          } else {
+            days += `<div class="day ">${i}</div>`;
+          }
+        }
+      }
+    
     for(let j =1; j <= nextDays; j++){
         days += `<div class="day next-date">${j}</div>`;
     }
 
     daysContainer.innerHTML = days;
+
+    addListener();
 }
 
 initCalendar();
@@ -135,4 +172,70 @@ function gotoDate() {
     alert("Invalid Date");
   }
 
-  
+  addEventBtn.addEventListener("click", () =>{
+    addEventWrapper.classList.toggle("active");
+  });
+
+  addEventCloseBtn.addEventListener("click", () =>{
+    addEventWrapper.classList.remove("active");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target !== addEventBtn && !addEventWrapper.contains(e.target)) {
+      addEventWrapper.classList.remove("active");
+    }
+  });
+
+  addEventTitle.addEventListener("input", (e) =>{
+    addEventTitle.value = addEventTitle.value.slice(0,50);
+  });
+
+  addEventFrom.addEventListener("input", (e) =>{
+    addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
+    if (addEventFrom.value.length ===2){
+        addEventFrom.value += ":";
+    }
+    if(addEventFrom.value.length > 5){
+        addEventFrom.value = addEventFrom.value.slice(0,5);
+    }
+  });
+
+  addEventTo.addEventListener("input", (e) =>{
+    addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, "");
+    if (addEventTo.value.length ===2){
+        addEventTo.value += ":";
+    }
+    if(addEventTo.value.length > 5){
+        addEventTo.value = addEventTo.value.slice(0,5);
+    }
+  });
+
+  function addListener(){
+    const days = document.querySelectorAll(".day");
+    days.forEach((day) =>{
+        day.addEventListener("click", (e) =>{
+            activeDay = Number(e.target.innerHTML)
+
+            days.forEach((day)=>{
+                day.classList.remove("active");
+            });
+
+
+            if(e.target.classList.contains("prev-date")){
+                prevMonth();
+
+                setTimeout(()=>{
+                    const days = document.querySelectorAll(".day");
+                    days.forEach((day)=>{
+                        if(
+                            !day.classList.contains("prev-date") &&
+                            day.innerHTML === e.target.innerHTML
+                        ){
+                            day.classList.add("active");
+                        }
+                    });
+                }, 100);
+            }
+        });
+    });
+  }
